8efc385c8a5786ffa735e255e3d931d80bacfbf3
Luiz Paulo Terra Nogueira <119353957+gueroso1@users.noreply.github.com>
2022-12-23 15:45:20 -0300

    ex017

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,31 @@
+function contar() {
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        RegExp.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if(i < f) {
+            for(let c = i;c <= f;c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i;c >= f;c -=  p) {
+                res.innerHTML +=  `${c} \u{1F449}`
+            }
+        }
+
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
