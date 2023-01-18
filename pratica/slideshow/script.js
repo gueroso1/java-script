ca40ffe35328ca2317ed07c3af0e0a91e5d62124
Luiz Paulo Terra Nogueira <119353957+gueroso1@users.noreply.github.com>
2023-01-18 17:13:48 -0300

    slideshow

---
+++ pratica/slideshow/script.js
@@ -0,0 +1,16 @@
+let containerItens = document.querySelector('#container-itens');
+let itens = document.querySelectorAll('.item')
+
+const anterior = () => {
+    containerItens.appendChild(itens[0]);
+    itens = document.querySelectorAll('.item')
+}
+
+const proximo = () => {
+    const lastItem = itens[itens.length - 1];
+    containerItens.insertBefore( lastItem, itens[0]);
+    itens = document.querySelectorAll('.item')
+}
+
+document.querySelector('#anterior').addEventListener('click', proximo)
+document.querySelector('#proximo').addEventListener('click', anterior)
