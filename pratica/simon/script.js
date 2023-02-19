9dc884f85f6883854d22e7bce69bdde83340fb3a
Luiz Paulo Terra Nogueira <119353957+gueroso1@users.noreply.github.com>
2023-02-18 22:15:20 -0300

    som

--- pratica/simon/script.js
+++ pratica/simon/script.js
@@ -5,9 +5,17 @@ const yellow = document.querySelector('#yellow');
 
 const score = document.querySelector('#score');
 const iniciar = document.querySelector('#start');
+const muteButton = document.querySelector('#mute');
+
+const somGameOver = new Audio('sounds/sounds_end.mp3');
+const redSom = new Audio('sounds/sounds_red.mp3');
+const greenSom = new Audio('sounds/sounds_green.mp3');
+const blueSom = new Audio('sounds/sounds_blue.mp3');
+const yellowSom = new Audio('sounds/sounds_red.mp3');
 
 let proximoIndice = 0;
 scoreAtual = 0;
+
 
 // Definir a sequência de botões aleatórios
 const cores = ['red', 'green', 'yellow', 'blue'];
@@ -20,6 +28,23 @@ const corEscolhida = () => {
     corAleatoria = Math.floor(Math.random() * 4);
 
     sequenciaCor.push(cores[corAleatoria]);
+
+};
+
+
+const tocarSom = (som) => {
+
+    console.log(som)
+
+    if (som === 'red') {
+        redSom.play();
+    } else if (som === 'green') {
+        greenSom.play();
+    } else if (som === 'blue') {
+        blueSom.play();
+    } else if (som === 'yellow') {
+        yellowSom.play();
+    }
 
 };
 
@@ -37,7 +62,6 @@ const checarResposta = (e) => {
         piscarCorSelecionada(corSelecionada.id);
 
 
-
         if (proximoIndice === sequenciaCor.length) {
 
             proximoIndice = 0;
@@ -49,12 +73,14 @@ const checarResposta = (e) => {
             setTimeout(() => {
                 piscarCor();
             }, 1000);
-            
+
 
         };
 
 
     } else {
+
+        somGameOver.play();
 
         alert('GAME OVER');
 
@@ -79,6 +105,8 @@ const piscarCor = () => {
 
         setTimeout(() => {
 
+            tocarSom(cor);
+
             document.querySelector(`#${cor}`).style.backgroundColor = '#fff';
 
             setTimeout(() => {
@@ -102,9 +130,14 @@ const piscarCor = () => {
 
 const piscarCorSelecionada = (id) => {
 
+    tocarSom(id);
+
     document.querySelector(`#${id}`).style.backgroundColor = '#fff';
+
     setTimeout(() => {
+
         document.querySelector(`#${id}`).style.backgroundColor = id;
+
     }, 300);
 
 };
@@ -121,3 +154,29 @@ iniciar.addEventListener('click', () => {
 
     console.log(sequenciaCor);
 })
+
+// evento para deixar o volume do som mudo
+muteButton.addEventListener('click', () => {
+
+    if (redSom.volume === 0) {
+
+        redSom.volume = 1;
+
+        greenSom.volume = 1;
+
+        blueSom.volume = 1;
+
+        yellowSom.volume = 1;
+
+    } else {
+
+        redSom.volume = 0;
+
+        greenSom.volume = 0;
+
+        blueSom.volume = 0;
+
+        yellowSom.volume = 0;
+    }
+
+});
